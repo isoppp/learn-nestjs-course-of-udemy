6c40a490a079a530e43b172abe5ee0d22aaa428f
isoppp <prog@d2deck.com>
2019-06-29 13:35:09 +0800

    Add TaskStatusValidationPipe

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -14,6 +14,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -40,7 +41,10 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
-  patchTask(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+  patchTask(
+    @Param('id') id: string,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
 
