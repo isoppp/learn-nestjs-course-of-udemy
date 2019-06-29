2664853df68b21a87c7147f8948367f091f6e008
isoppp <prog@d2deck.com>
2019-06-29 13:28:16 +0800

    Add not found exeption to deleteTask

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -61,6 +61,7 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 }
