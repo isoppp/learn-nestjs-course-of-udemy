5d21bafbbc7100376c5f4dafdb586dfe328e0be6
isoppp <prog@d2deck.com>
2019-08-30 13:28:14 -0700

    Add comment

--- frontend/src/services/base-http.service.js
+++ frontend/src/services/base-http.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default class BaseHttpService {
-  BASE_URL = 'http://localhost:3000';
+  BASE_URL = 'http://localhost:3000'; // need changes when deploy production
   _accessToken = null;
 
   constructor(routerStore) {
@@ -17,19 +17,19 @@ export default class BaseHttpService {
   async post(endpoint, data = {}, options = {}) {
     Object.assign(options, this._getCommonOptions());
     return axios.post(`${this.BASE_URL}/${endpoint}`, data, options)
-      .catch(error => this._handleHttpError(error));  
+      .catch(error => this._handleHttpError(error));
   }
 
   async delete(endpoint, options = {}) {
     Object.assign(options, this._getCommonOptions());
     return axios.delete(`${this.BASE_URL}/${endpoint}`, options)
-      .catch(error => this._handleHttpError(error));     
+      .catch(error => this._handleHttpError(error));
   }
 
   async patch(endpoint, data = {}, options = {}) {
     Object.assign(options, this._getCommonOptions());
     return axios.patch(`${this.BASE_URL}/${endpoint}`, data, options)
-      .catch(error => this._handleHttpError(error));   
+      .catch(error => this._handleHttpError(error));
   }
 
   _handleHttpError(error) {
