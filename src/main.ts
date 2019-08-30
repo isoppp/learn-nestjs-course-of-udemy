03112c50f993a307d981cc9326a47880b8cb9992
isoppp <prog@d2deck.com>
2019-08-30 13:20:02 -0700

    Prepare for production

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,11 @@ async function bootstrap() {
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
+  } else {
+    app.enableCors({
+      origin: serverConfig.origin,
+    });
+    logger.log(`Accepting requests from origin ${serverConfig.origin}`);
   }
 
   const port = process.env.PORT || serverConfig.port;
